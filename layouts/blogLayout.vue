16828f9becd89d886144c8374427f92349c35c6d
messabih-khalil <msbih.khalil@gmail.com>
2022-11-21 18:56:03 +0100

    Custom Layout

---
+++ layouts/blogLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav>
+      <p class="logo">Logo Blog</p>
+    </nav>
+
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+nav {
+  background-color: red;
+  .logo {
+    color: white;
+    font-size: 2rem;
+    padding: 0.5rem;
+  }
+}
+</style>
